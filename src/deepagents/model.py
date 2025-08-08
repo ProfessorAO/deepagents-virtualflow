00db28e30b7cdec68dd9647706b2ae58da5d725b
ProfessorAO <davidodunlade@hotmail.co.uk>
2025-08-08 13:18:54 +0100

    reduced deafult model temp for structuring and increase normal model temp

--- src/deepagents/model.py
+++ src/deepagents/model.py
@@ -6,4 +6,4 @@ def get_default_model():
     return ChatFireworks(model="accounts/fireworks/models/kimi-k2-instruct", temperature=0.1, max_tokens=64000, max_retries=3)
 
 def get_default_structuring_model():
-    return ChatFireworks(model="accounts/fireworks/models/llama4-maverick-instruct-basic", temperature=0.1, max_tokens=128000, max_retries=3)
+    return ChatFireworks(model="accounts/fireworks/models/llama4-maverick-instruct-basic", temperature=0, max_tokens=128000, max_retries=3)
